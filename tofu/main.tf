bd31dac229a117d43e700c51227e0284a029e9d5
abner espinoza <abespinoza155@gmail.com>
2025-08-05 08:32:54 -0400

    initial_commit

---
+++ tofu/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+# Build Docker image from Dockerfile
+resource "docker_image" "custom_ubuntu" {
+  name = "custom_ubuntu:latest"
+  build {
+    context    = "${path.module}/../docker"
+    dockerfile = "Dockerfile"
+  }
+}
+
+# Create container from custom image
+resource "docker_container" "ubuntu_container" {
+  name  = "ubuntu_ansible_ready"
+  image = docker_image.custom_ubuntu.latest
+  tty   = true
+
+  ports {
+    internal = 22
+    external = 2222
+  }
+
+  # Optional: auto-remove stopped container (uncomment if needed)
+  # must_run = false
+  # restart = "no"
+}
+
+# Optional output to display SSH connection info
+output "ssh_connection_command" {
+  value = "ssh root@localhost -p 2222"
+}
+
+output "container_id" {
+  value = docker_container.ubuntu_container.id
+}
